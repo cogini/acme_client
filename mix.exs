770a18784b7864418170149a1ec2372669b297b1
Jake Morrison <jake@cogini.com>
2021-01-20 16:59:50 +0800

    Add basic libs

--- mix.exs
+++ mix.exs
@@ -1,28 +1,79 @@
 defmodule AcmeClient.MixProject do
   use Mix.Project
 
+  @github "https://github.com/cogini/acme_client"
+
   def project do
     [
       app: :acme_client,
       version: "0.1.0",
       elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      source_url: @github,
+      homepage_url: @github,
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      dialyzer: [
+        # plt_add_apps: [:erlavro, :tesla],
+        # plt_add_deps: true,
+        # flags: ["-Werror_handling", "-Wrace_conditions"],
+        flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs],
+        # ignore_warnings: "dialyzer.ignore-warnings"
+      ],
+      deps: deps(),
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger] ++ extra_applications(Mix.env())
     ]
   end
+
+  defp extra_applications(:test), do: [:hackney]
+  defp extra_applications(_),     do: []
+
+  # Specifies which paths to compile per environment
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.23.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.13.4", only: [:dev, :test], runtime: false},
+      {:hackney, "~> 1.17", only: [:dev, :test]},
+      {:jason, "~> 1.0"},
+      {:telemetry, "~> 0.4.2"},
+      {:tesla, "~> 1.4"},
+    ]
+  end
+
+  defp description do
+    "Client for ACME certificate management protocol RFC8555"
+  end
+
+  defp package do
+    [
+      maintainers: ["Jake Morrison"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => @github}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url: @github,
+      extras: ["README.md", "CHANGELOG.md"],
+      # api_reference: false,
+      source_url_pattern: "#{@github}/blob/master/%{path}#L%{line}",
     ]
   end
 end
