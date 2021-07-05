74571bf1710735a5ac738705e7c2401f2c72b265
Jake Morrison <jake@cogini.com>
2021-07-05 15:26:07 -0500

    Add account_kid

--- lib/acme_client/session.ex
+++ lib/acme_client/session.ex
@@ -1,10 +1,11 @@
 defmodule AcmeClient.Session do
   @moduledoc """
-  This stores information about the request.
+  This stores information about the current session.
   """
 
   defstruct [
     account_key: nil,
+    account_kid: nil,
     client: nil,
     nonce: "",
     directory: %{}, # url map
@@ -12,6 +13,7 @@ defmodule AcmeClient.Session do
 
   @type t :: %__MODULE__{
     account_key: JOSE.JWK.t(),
+    account_kid: binary(),
     client: Tesla.Client.t(),
     nonce: binary(),
     directory: map(),
