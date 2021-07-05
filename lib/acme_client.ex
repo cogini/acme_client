557b463563c7fdfca7ec154f8a73f19e544a2fcf
Jake Morrison <jake@cogini.com>
2021-07-05 15:26:51 -0500

    Implement new_order

--- lib/acme_client.ex
+++ lib/acme_client.ex
@@ -4,6 +4,7 @@ defmodule AcmeClient do
   """
   require Logger
   alias AcmeClient.Session
+  @app :acme_client
 
   @type code :: non_neg_integer()
   @type client :: Tesla.Env.client()
@@ -53,7 +54,8 @@ defmodule AcmeClient do
     opts_middleware = opts[:middleware] || []
     adapter = opts[:adapter]
     session = %Session{
-      account_key: opts[:account_key]
+      account_key: opts[:account_key],
+      account_kid: opts[:account_kid]
     }
 
     middleware = opts_middleware ++ [
@@ -153,7 +155,7 @@ defmodule AcmeClient do
 
     payload =
       opts
-      |> Enum.reduce(%{}, &reduce_account_params/2)
+      |> Enum.reduce(%{}, &reduce_new_account_opts/2)
       |> Jason.encode!()
 
     protected = %{"alg" => "ES256", "nonce" => nonce, "url" => url, jwk: to_jwk(account_key)}
@@ -164,7 +166,7 @@ defmodule AcmeClient do
       {:ok, %{status: status, headers: headers} = result} when status in [201, 200] ->
         session = set_nonce(session, headers)
         value = %{
-          account: result.body,
+          object: result.body,
           location: :proplists.get_value("location", headers, nil)
         }
         {:ok, session, value}
@@ -175,37 +177,174 @@ defmodule AcmeClient do
     end
   end
 
-  @spec update_nonce(Session.t(), headers()) :: Session.t()
-  def update_nonce(session, headers) do
-    %{session | nonce: :proplists.get_value("replay-nonce", headers)}
+  # Convert new_account/2 params to name/format required by API
+  defp reduce_new_account_opts({:contact, value}, acc) when is_list(value) do
+    Map.put(acc, "contact", value)
   end
 
-  @spec extract_nonce(headers()) :: binary() | nil
-  def extract_nonce(headers) do
-    :proplists.get_value("replay-nonce", headers, nil)
+  defp reduce_new_account_opts({:contact, value}, acc) when is_binary(value) do
+    Map.put(acc, "contact", [value])
+  end
+
+  defp reduce_new_account_opts({:terms_of_service_agreed, true}, acc) do
+    Map.put(acc, "termsOfServiceAgreed", true)
+  end
+
+  defp reduce_new_account_opts({:only_return_existing, true}, acc) do
+    Map.put(acc, "onlyReturnExisting", true)
+  end
+
+  defp reduce_new_account_opts({:external_account_binding, value}, acc) do
+    Map.put(acc, "externalAccountBinding", value)
+  end
+
+  defp reduce_new_account_opts(_, acc), do: acc
+
+  @doc ~S"""
+  Create new order.
+
+  Params:
+
+  * identifiers: domain(s), either binary value or type/value map
+  * not_before: datetime in RFC3339 (ISO8601) format (optional)
+  * not_after: datetime in RFC3339 (ISO8601) format (optional)
+
+  `account_key` and `account_kid` must be set in session.
+  """
+  @spec new_order(Session.t(), Keyword.t()) :: {:ok, Session.t(), map()} | {:error, Session.t(), Tesla.Env.result()}
+  def new_order(session, opts) do
+    %{client: client, directory: directory, account_key: account_key, account_kid: kid, nonce: nonce} = session
+    url = directory["newOrder"]
+    req_headers = [{"content-type", "application/jose+json"}]
+
+    payload =
+      opts
+      |> Enum.reduce(%{}, &reduce_new_order_opts/2)
+      |> Jason.encode!()
+
+    protected = %{"alg" => "ES256", "kid" => kid, "nonce" => nonce, "url" => url}
+    {_, body} = JOSE.JWS.sign(account_key, payload, protected)
+
+    case Tesla.request(client, method: :post, url: url, body: body, headers: req_headers) do
+      {:ok, %{status: status, headers: headers} = result} when status in [201] ->
+        session = set_nonce(session, headers)
+        value = %{
+          object: result.body,
+          location: :proplists.get_value("location", headers, nil)
+        }
+        {:ok, session, value}
+      {:ok, %{headers: headers} = result} ->
+        {:error, set_nonce(session, headers), result}
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  # Convert new_account/2 params to name/format required by API
+  defp reduce_new_order_opts({:identifiers, value}, acc) when is_binary(value) do
+    Map.put(acc, "identifiers", [%{type: "dns", value: value}])
+  end
+
+  defp reduce_new_order_opts({:identifiers, value}, acc) when is_map(value) do
+    Map.put(acc, "identifiers", [value])
+  end
+
+  defp reduce_new_order_opts({:identifiers, values}, acc) when is_list(values) do
+    Map.put(acc, "identifiers", Enum.map(values, &convert_new_order_identifier/1))
+  end
+
+  defp reduce_new_order_opts({:not_before, value}, acc) when is_binary(value) do
+    Map.put(acc, "notBefore", value)
+  end
+
+  defp reduce_new_order_opts({:not_after, value}, acc) when is_binary(value) do
+    Map.put(acc, "notAfter", value)
+  end
+
+  defp reduce_new_order_opts(_, acc), do: acc
+
+  defp convert_new_order_identifier(value) when is_binary(value) do
+    %{type: "dns", value: value}
+  end
+
+  defp convert_new_order_identifier(value) when is_map(value) do
+    value
   end
 
   @doc ~S"""
-  Create client to talk to server..
+  Set up session then create account."
 
-  Options are:
+  Params:
 
-  * base_url: URL of server (optional, default "http://localhost:8081")
+  * account_key: Account key
+  * contact: Account owner contact
 
   ## Examples
 
-      iex> client = AcmeClient.new()
+    contact = "mailto:admin@example.com"
+    {:ok, account_key} = AcmeClient.generate_account_key()
+    {:ok, session, account} = AcmeClient.create_account(account_key: account_key, contact: contact)
+  """
+  @spec create_account(Keyword.t()) :: request_ret()
+  def create_account(params) do
+    with {:ok, session} <- new_session(),
+         {:ok, session} <- new_nonce(session)
+    do
+      session = %{session | account_key: params[:account_key]}
+      default_params = [
+        terms_of_service_agreed: true
+      ]
+      params = Keyword.merge(default_params, params)
+      new_account(session, params)
+    end
+  end
+
+  @spec create_session(Keyword.t()) :: {:ok, Session.t()} | {:error, term()}
+  def create_session(params \\ []) do
+    account_key =
+      case Keyword.fetch(params, :account_key) do
+        {:ok, value} ->
+          value
+        :error ->
+          account_key_bin = Application.get_env(@app, :account_key)
+          AcmeClient.binary_to_key(account_key_bin)
+      end
+
+    account_kid =
+      case Keyword.fetch(params, :account_kid) do
+        {:ok, value} ->
+          value
+        :error ->
+          Application.get_env(@app, :account_kid)
+      end
+
+    {:ok, session} = new_session(account_key: account_key, account_kid: account_kid)
+    new_nonce(session)
+  end
+
+  @doc ~S"""
+  Create Tesla client.
+
+  Options are:
+
+  * base_url: URL of server (optional), default "https://acme-staging-v02.api.letsencrypt.org/directory"
+
+  ## Examples
+
+      iex> client = AcmeClient.create_client()
       %Tesla.Client{
         adapter: nil,
         fun: nil,
         post: [],
         pre: [
-          {Tesla.Middleware.BaseUrl, :call, ["http://localhost:8081"]},
+          {Tesla.Middleware.BaseUrl, :call,
+           ["https://acme-staging-v02.api.letsencrypt.org/directory"]},
+          {Tesla.Middleware.JSON, :call, [[]]}
         ]
       }
   """
-  @spec new(Keyword.t()) :: Tesla.Client.t()
-  def new(opts \\ []) do
+  @spec create_client(Keyword.t()) :: Tesla.Client.t()
+  def create_client(opts \\ []) do
     base_url = opts[:base_url] || "https://acme-staging-v02.api.letsencrypt.org/directory"
     opts_middleware = opts[:middleware] || []
     adapter = opts[:adapter]
@@ -225,37 +364,35 @@ defmodule AcmeClient do
 
   # Internal utility functions
 
-  # Convert new_account/2 params to name/format required by API
-  defp reduce_account_params({:contact, value}, acc) when is_list(value) do
-    Map.put(acc, "contact", value)
+  @spec update_nonce(Session.t(), headers()) :: Session.t()
+  def update_nonce(session, headers) do
+    %{session | nonce: :proplists.get_value("replay-nonce", headers)}
   end
 
-  defp reduce_account_params({:contact, value}, acc) when is_binary(value) do
-    Map.put(acc, "contact", [value])
+  @spec extract_nonce(headers()) :: binary() | nil
+  def extract_nonce(headers) do
+    :proplists.get_value("replay-nonce", headers, nil)
   end
 
-  defp reduce_account_params({:terms_of_service_agreed, true}, acc) do
-    Map.put(acc, "termsOfServiceAgreed", true)
-  end
-
-  defp reduce_account_params({:only_return_existing, true}, acc) do
-    Map.put(acc, "onlyReturnExisting", true)
-  end
-
-  defp reduce_account_params({:external_account_binding, value}, acc) do
-    Map.put(acc, "externalAccountBinding", value)
-  end
-
-  defp reduce_account_params(_, acc), do: acc
-
-  # Convert key to JWK representation
+  # Convert key to JWK representation used in API
   defp to_jwk(account_key) do
     {_modules, public_map} = JOSE.JWK.to_public_map(account_key)
     public_map
   end
 
+  # Convert key struct to binary
+  def key_to_binary(key) do
+    {_type, value} = JOSE.JWK.to_binary(key)
+    value
+  end
+
+  # Convert binary to key struct
+  def binary_to_key(bin) do
+    JOSE.JWK.from_binary(bin)
+  end
+
   @spec do_get(Tesla.Client.t(), binary()) :: {:ok, map()} | {:error, map()}
-  defp do_get(client, url) do
+  def do_get(client, url) do
     tesla_response(Tesla.get(client, url))
   end
 
@@ -292,33 +429,4 @@ defmodule AcmeClient do
   defp set_nonce(session, headers) do
     %{session | nonce: extract_nonce(headers)}
   end
-
-  @doc ~S"""
-  Set up session then create account."
-
-  Params:
-
-  * account_key: Account key
-  * contact: Account owner contact
-
-  ## Examples
-
-    contact = "mailto:admin@example.com"
-    {:ok, account_key} = AcmeClient.generate_account_key()
-    {:ok, session, account} = AcmeClient.create_account(account_key: account_key, contact: contact)
-  """
-  @spec create_account(Keyword.t()) :: request_ret()
-  def create_account(params) do
-    with {:ok, session} <- new_session(),
-         {:ok, session} <- new_nonce(session)
-    do
-      session = %{session | account_key: params[:account_key]}
-      default_params = [
-        terms_of_service_agreed: true
-      ]
-      params = Keyword.merge(default_params, params)
-      new_account(session, params)
-    end
-  end
-
 end
