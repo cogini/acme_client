51834222a4c4406c0b71208177fd2f9bfc1991d8
Jake Morrison <jake@cogini.com>
2021-07-06 12:28:09 -0500

    Create challenge responses for order

--- lib/acme_client.ex
+++ lib/acme_client.ex
@@ -31,22 +31,27 @@ defmodule AcmeClient do
   @doc ~S"""
   Create new session connecting to ACME server."
 
-  This sets up Tesla to talk to the server, then reads the directory URL.
+  Sets up the Tesla client library, then reads the server's directory URL,
+  which maps standard names for operations to the specific URLs on the server.
 
   Params:
 
-  * directory_url: Server directory URL, which gives references to other endpoints.
-                   Defaults to `https://acme-staging-v02.api.letsencrypt.org/directory`.
-                   For production, `https://acme-v02.api.letsencrypt.org/directory`
+  * directory_url: Server directory URL.
+                   Defaults to staging server `https://acme-staging-v02.api.letsencrypt.org/directory`.
+                   Production is `https://acme-v02.api.letsencrypt.org/directory`
   * middleware: Tesla middlewares (optional)
   * adapter: Tesla adapter (optional)
   * account_key: ACME account key (optional)
+  * account_kid: ACME account key id, a URL (optional)
 
   ## Examples
 
-    contact = "mailto:admin@example.com"
     {:ok, account_key} = AcmeClient.generate_account_key()
-    {:ok, session, account} = AcmeClient.create_account(account_key: account_key, contact: contact)
+    contact = "mailto:admin@example.com"
+    {:ok, session, account} = AcmeClient.new_secreate_account(account_key: account_key, contact: contact)
+
+    {:ok, session} = AcmeClient.new_session(account_key: account_key, account_kid: account_kid)
+    {:ok, session} = AcmeClient.new_nonce(session)
   """
   @spec new_session(Keyword.t()) :: {:ok, Session.t()} | {:error, term()}
   def new_session(opts \\ []) do
@@ -55,7 +60,7 @@ defmodule AcmeClient do
     adapter = opts[:adapter]
     session = %Session{
       account_key: opts[:account_key],
-      account_kid: opts[:account_kid]
+      account_kid: opts[:account_kid],
     }
 
     middleware = opts_middleware ++ [
@@ -76,8 +81,6 @@ defmodule AcmeClient do
     end
   end
 
-  # body: "{\n  \"type\": \"urn:ietf:params:acme:error:malformed\",\n  \"detail\": \"Request payload did not parse as JSON\",\n  \"status\": 400\n}",
-
   @doc "Get nonce from server and add to session"
   @spec new_nonce(Session.t()) :: {:ok, Session.t()} | {:error, term()}
   def new_nonce(session) do
@@ -92,98 +95,117 @@ defmodule AcmeClient do
     end
   end
 
-  @doc "Generate cryptographic key for account"
-  @spec generate_account_key(Keyword.t()) :: {:ok, JOSE.JWK.t()}
-  def generate_account_key(params \\ []) do
-    alg = params[:alg] || "ES256"
-    {:ok, JOSE.JWS.generate_key(%{"alg" => alg})}
-  end
-  # def generate_account_key(params) do
-  #   key_size = params[:key_size] || 2048
-  #   JOSE.JWK.generate_key({:rsa, key_size})
-  # end
-
-  # HTTP challenge
-  # url = "/.well-known/acme-challenge/" <> token
-  #
-  # Response
-  # HTTP/1.1 200 OK
-  # Content-Type: application/octet-stream
-  #
-  # <key_authorization>
-
-  # DNS challenge
-  # _acme-challenge.www.example.org. 300 IN TXT "<key_authorization>"
-
   @doc ~S"""
-  Create key authorization from token and key.
+  Convenience function which creates a session.
 
-  https://datatracker.ietf.org/doc/html/rfc8555#section-8.1
-  """
-  @spec key_authorization(binary(), JOSE.JWK.t()) :: binary()
-  def key_authorization(token, key) do
-    token <> "." <> key_thumbprint(key)
-  end
+  Params:
+    * directory_url (optional)
+    * account_key: Account key (optional)
+    * account_kid: Account key id URL (optional)
 
-  @doc ~S"""
-  Generate RFC7638 thumbprint of key.
+  If params are not specified, they are read from the environment.
 
   ## Examples
 
-    AcmeClient.key_thumbprint(session.account_key)
+    {:ok, session} = AcmeClient.create_session()
   """
-  @spec key_thumbprint(JOSE.JWK.t()) :: binary()
-  def key_thumbprint(key) do
-    key
-    |> JOSE.JWK.to_thumbprint_map()
-    |> JOSE.JWK.thumbprint()
+  @spec create_session(Keyword.t()) :: {:ok, Session.t()} | {:error, term()}
+  def create_session(params \\ []) do
+    url =
+      case Keyword.fetch(params, :directory_url) do
+        {:ok, value} ->
+          value
+        :error ->
+          Application.get_env(@app, :directory_url)
+      end
+
+    key =
+      case Keyword.fetch(params, :account_key) do
+        {:ok, value} ->
+          value
+        :error ->
+          account_key_bin = Application.get_env(@app, :account_key)
+          AcmeClient.binary_to_key(account_key_bin)
+      end
+
+    kid =
+      case Keyword.fetch(params, :account_kid) do
+        {:ok, value} ->
+          value
+        :error ->
+          Application.get_env(@app, :account_kid)
+      end
+
+    {:ok, session} = new_session(directory_url: url, account_key: key, account_kid: kid)
+    new_nonce(session)
   end
+
 
   @doc ~S"""
-  Generate DNS validation value.
+  Perform POST-as-GET HTTP call.
 
-  https://datatracker.ietf.org/doc/html/rfc8555#section-8.4
-
-  """
-  def dns_validation(token, key) do
-    token
-    |> key_authorization(key)
-    |> (&(:crypto.hash(:sha256, &1))).()
-    |> Base.url_encode64(case: :lower)
-    # |> Base.url_encode64(padding: false, case: :lower)
-  end
-
-  # %{
-  #   "contact" => ["mailto:jake@cogini.com"],
-  #   "createdAt" => "2021-01-21T02:25:34.191981376Z",
-  #   "initialIp" => "123.194.199.220",
-  #   "key" => %{
-  #     "alg" => "ES256",
-  #     "crv" => "P-256",
-  #     "kty" => "EC",
-  #     "use" => "sig",
-  #     "x" => "kk1Lezgf2nsLAc2_Is8pP2KGJRTvTBF2EfPpJgRxWuo",
-  #     "y" => "iDsb47bohf2_HMTfo5BGwp4PrjGce7jicc7Jix4B5Yg"
-  #   },
-  #   "status" => "valid"
-  # }}
+  This reads a URL from the server. Instead of using GET, it uses POST so that the
+  request has the proper signing and nonce.
   #
-  # %{
-  #   "detail" => "must agree to terms of service",
-  #   "status" => 400,
-  #   "type" => "urn:ietf:params:acme:error:malformed"
-  # }
+  ## Examples
+    {:ok, session, response} = AcmeClient.post_as_get(session, "https://acme-staging-v02.api.letsencrypt.org/acme/acct/123")
+  """
+  @spec post_as_get(Session.t(), binary()) :: {:ok, Session.t(), term()} | {:error, term()}
+  def post_as_get(session, url, payload \\ "") do
+    %{client: client, account_key: account_key, account_kid: kid, nonce: nonce} = session
+    req_headers = [{"content-type", "application/jose+json"}]
+
+    protected = %{"alg" => "ES256", "kid" => kid, "nonce" => nonce, "url" => url}
+    {_, body} = JOSE.JWS.sign(account_key, payload, protected)
+
+    case Tesla.request(client, method: :post, url: url, body: body, headers: req_headers) do
+      {:ok, %{status: 200, headers: headers} = result} ->
+        session = set_nonce(session, headers)
+        {:ok, session, result}
+      {:ok, %{headers: headers} = result} ->
+        {:error, set_nonce(session, headers), result}
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  @doc "Get a list of URLs with post_as_get"
+  @spec get_urls(Session.t(), list(binary())) :: {:ok, Session.t(), term()}
+  def get_urls(session, urls) do
+    {session, results} =
+      Enum.reduce(urls, {session, []},
+        fn url, {session, acc} ->
+          {:ok, session, result} = AcmeClient.post_as_get(session, url)
+          {session, [{url, result.body} | acc]}
+        end)
+    {:ok, session, Enum.reverse(results)}
+  end
 
   @doc ~S"""
-  Create new account.
+  Generate JWS cryptographic key for account.
+  """
+  @spec generate_account_key(Keyword.t()) :: {:ok, JOSE.JWK.t()}
+  def generate_account_key(opts \\ []) do
+    alg = opts[:alg] || "ES256"
+    {:ok, JOSE.JWS.generate_key(%{"alg" => alg})}
+  end
+
+  # def generate_account_key(opts) do
+  #   key_size = opts[:key_size] || 2048
+  #   JOSE.JWK.generate_key({:rsa, key_size})
+  # end
+
+
+  @doc ~S"""
+  Create new ACME account.
 
   Params:
-
-  * account_key: Account key, from `generate_account_key/1`
-  * contact: Account owner contact(s), e.g. "mailto:jake@cogini.com" (optional)
-  * terms_of_service_agreed: true (optional)
-  * only_return_existing: true (optional)
-  * external_account_binding: associated external account (optional)
+    * account_key: Account key, from `generate_account_key/1`
+    * contact: Account owner contact(s), e.g. "mailto:jake@cogini.com", string
+               or array of strings.
+    * terms_of_service_agreed: true (optional)
+    * only_return_existing: true (optional)
+    * external_account_binding: associated external account (optional)
 
   ## Examples
 
@@ -197,7 +219,7 @@ defmodule AcmeClient do
     {:ok, session} = new_nonce(session)
     {:ok, session, account} = AcmeClient.create_account(params)
   """
-  @spec new_account(Session.t(), Keyword.t()) :: {:ok, Session.t(), map()} | {:error, Session.t(), Tesla.Env.result()}
+  @spec new_account(Session.t(), Keyword.t()) :: {:ok, Session.t(), map()} | {:error, Session.t(), Tesla.Env.result()} | {:error, term()}
   def new_account(session, opts) do
     %{client: client, directory: directory, account_key: account_key, nonce: nonce} = session
     url = directory["newAccount"]
@@ -249,6 +271,70 @@ defmodule AcmeClient do
   end
 
   defp reduce_new_account_opts(_, acc), do: acc
+
+
+
+  @doc ~S"""
+  Create HTTP challenge URL for token.
+
+  https://datatracker.ietf.org/doc/html/rfc8555#section-8.3
+
+  Response:
+
+    HTTP/1.1 200 OK
+    Content-Type: application/octet-stream
+
+    <key_authorization>
+  """
+  @spec http_challenge_url(binary()) :: binary()
+  def http_challenge_url(token) do
+    "/.well-known/acme-challenge/" <> token
+  end
+
+
+  @doc ~S"""
+  Create key authorization from token and key.
+
+  https://datatracker.ietf.org/doc/html/rfc8555#section-8.1
+  """
+  @spec key_authorization(binary(), JOSE.JWK.t()) :: binary()
+  def key_authorization(token, key) do
+    token <> "." <> key_thumbprint(key)
+  end
+
+
+  @doc ~S"""
+  Generate RFC7638 thumbprint of key.
+
+  https://datatracker.ietf.org/doc/html/rfc7638
+
+  ## Examples
+
+    AcmeClient.key_thumbprint(account_key)
+  """
+  @spec key_thumbprint(JOSE.JWK.t()) :: binary()
+  def key_thumbprint(key) do
+    key
+    |> JOSE.JWK.to_thumbprint_map()
+    |> JOSE.JWK.thumbprint()
+  end
+
+
+  @doc ~S"""
+  Generate DNS challenge response.
+
+  https://datatracker.ietf.org/doc/html/rfc8555#section-8.4
+
+    _acme-challenge.www.example.org. 300 IN TXT "<key_authorization>"
+
+  """
+  def dns_challenge_response(token, key) do
+    token
+    |> key_authorization(key)
+    |> (&(:crypto.hash(:sha256, &1))).()
+    |> Base.url_encode64(padding: false, case: :lower)
+  end
+
 
   @doc ~S"""
   Create new order.
@@ -321,22 +407,30 @@ defmodule AcmeClient do
     value
   end
 
-  def get_order_challenges(session, authorizations) do
-    {session, results} =
-      Enum.reduce(authorizations, {session, []},
-        fn url, {session, acc} ->
-          {:ok, session, result} = AcmeClient.post_as_get(session, url)
-          {session, [{url, result.body} | acc]}
-        end)
-    {:ok, session, Enum.reverse(results)}
+  # @spec create_challenge_responses(map()) ::
+  # %{
+  #     "authorizations" => ["https://acme-staging-v02.api.letsencrypt.org/acme/authz-v3/82803238",
+  #      "https://acme-staging-v02.api.letsencrypt.org/acme/authz-v3/82803239"],
+  #     "expires" => "2021-07-12T20:22:34Z",
+  #     "finalize" => "https://acme-staging-v02.api.letsencrypt.org/acme/finalize/20177848/94029681",
+  #     "identifiers" => [
+  #       %{"type" => "dns", "value" => "cogini.com"},
+  #       %{"type" => "dns", "value" => "www.cogini.com"}
+  #     ],
+  #     "status" => "pending"
+  # },
+  def create_challenge_responses(session, order_url) do
+    {:ok, session, order} = AcmeClient.post_as_get(session, order_url)
+    {:ok, session, authorizations} = AcmeClient.get_urls(session, order.body["authorizations"])
+    for {_url, authorization} <- authorizations do
+      authorization_to_challenge_response(authorization, session.account_key)
+    end
   end
 
-  def create_validations(challenge_objects, key) do
-    for {_url, %{"challenges" => challenges, "identifier" => %{"value" => domain}}} <- challenge_objects,
-      %{"type" => type, "token" => token} <- challenges,
-      type == "dns-01"
-    do
-      {domain, dns_validation(token, key)}
+  def authorization_to_challenge_response(authorization, key) do
+    %{"challenges" => challenges, "identifier" => %{"value" => domain}} = authorization
+    for %{"type" => type, "token" => token} <- challenges, type == "dns-01" do
+      {domain, dns_challenge_response(token, key)}
     end
   end
 
@@ -346,82 +440,13 @@ defmodule AcmeClient do
   # end
 
   @doc ~S"""
-  Set up session then create account."
-
-  Params:
-
-  * account_key: Account key
-  * contact: Account owner contact
-
-  ## Examples
-
-    contact = "mailto:admin@example.com"
-    {:ok, account_key} = AcmeClient.generate_account_key()
-    {:ok, session, account} = AcmeClient.create_account(account_key: account_key, contact: contact)
-  """
-  @spec create_account(Keyword.t()) :: request_ret()
-  def create_account(params) do
-    with {:ok, session} <- new_session(),
-         {:ok, session} <- new_nonce(session)
-    do
-      session = %{session | account_key: params[:account_key]}
-      default_params = [
-        terms_of_service_agreed: true
-      ]
-      params = Keyword.merge(default_params, params)
-      new_account(session, params)
-    end
-  end
-
-  @spec create_session(Keyword.t()) :: {:ok, Session.t()} | {:error, term()}
-  def create_session(params \\ []) do
-    account_key =
-      case Keyword.fetch(params, :account_key) do
-        {:ok, value} ->
-          value
-        :error ->
-          account_key_bin = Application.get_env(@app, :account_key)
-          AcmeClient.binary_to_key(account_key_bin)
-      end
-
-    account_kid =
-      case Keyword.fetch(params, :account_kid) do
-        {:ok, value} ->
-          value
-        :error ->
-          Application.get_env(@app, :account_kid)
-      end
-
-    {:ok, session} = new_session(account_key: account_key, account_kid: account_kid)
-    new_nonce(session)
-  end
-
-  @doc "Perform POST-as-GET HTTP call"
-  @spec post_as_get(Session.t(), binary()) :: {:ok, Session.t(), term()} | {:error, term()}
-  def post_as_get(session, url, payload \\ "") do
-    %{client: client, account_key: account_key, account_kid: kid, nonce: nonce} = session
-    req_headers = [{"content-type", "application/jose+json"}]
-
-    protected = %{"alg" => "ES256", "kid" => kid, "nonce" => nonce, "url" => url}
-    {_, body} = JOSE.JWS.sign(account_key, payload, protected)
-
-    case Tesla.request(client, method: :post, url: url, body: body, headers: req_headers) do
-      {:ok, %{status: 200, headers: headers} = result} ->
-        session = set_nonce(session, headers)
-        {:ok, session, result}
-      {:ok, %{headers: headers} = result} ->
-        {:error, set_nonce(session, headers), result}
-      {:error, reason} ->
-        {:error, reason}
-    end
-  end
-
-  @doc ~S"""
   Create Tesla client.
 
   Options are:
 
   * base_url: URL of server (optional), default "https://acme-staging-v02.api.letsencrypt.org/directory"
+  * adapter: HTTP client adapter (optional)
+  * middleware: Additional Tesla middleware modules (optional)
 
   ## Examples
 
@@ -440,9 +465,9 @@ defmodule AcmeClient do
   @spec create_client(Keyword.t()) :: Tesla.Client.t()
   def create_client(opts \\ []) do
     base_url = opts[:base_url] || "https://acme-staging-v02.api.letsencrypt.org/directory"
-    opts_middleware = opts[:middleware] || []
     adapter = opts[:adapter]
 
+    opts_middleware = opts[:middleware] || []
     middleware = opts_middleware ++ [
       {Tesla.Middleware.BaseUrl, base_url},
       Tesla.Middleware.JSON,
@@ -456,6 +481,7 @@ defmodule AcmeClient do
     do_get(client, "/directory")
   end
 
+
   # Internal utility functions
 
   # Set session nonce from server response headers
@@ -464,6 +490,7 @@ defmodule AcmeClient do
     %{session | nonce: extract_nonce(headers)}
   end
 
+  @doc "Get nonce from headers"
   @spec extract_nonce(headers()) :: binary() | nil
   def extract_nonce(headers) do
     :proplists.get_value("replay-nonce", headers, nil)
