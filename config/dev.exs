910876e0a36330297da101eaa6fce4acbf59ff88
Jake Morrison <jake@cogini.com>
2023-12-01 22:07:02 -0600

    Configure

--- config/dev.exs
+++ config/dev.exs
@@ -3,11 +3,6 @@ import Config
 config :tesla, adapter: Tesla.Adapter.Hackney
 
 config :acme_client,
-  # directory_url: "https://acme-staging-v02.api.letsencrypt.org/directory",
-  account_kid: "https://acme-staging-v02.api.letsencrypt.org/acme/acct/20177848",
-  account_key:
-    "{\"alg\":\"ES256\",\"crv\":\"P-256\",\"d\":\"x6jI_MoOzAChooOJaayBNXXE_wviurtWSsnoYUFnKL0\",\"kty\":\"EC\",\"use\":\"sig\",\"x\":\"AR0XIoSZ9zJnV-V_SHIdL9cmgPdyQqyzUW6SzSPfedQ\",\"y\":\"jzqO48YDlXfGEf4AEBf3zhN6vTc0aYFlGxzmYapY0Nw\"}"
-
-# prod
-# directory_url: "https://acme-v02.api.letsencrypt.org/directory",
-# directory: %{"keyChange" => "https://acme-v02.api.letsencrypt.org/acme/key-change", "meta" => %{"caaIdentities" => ["letsencrypt.org"], "termsOfService" => "https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf", "website" => "https://letsencrypt.org"}, "newAccount" => "https://acme-v02.api.letsencrypt.org/acme/new-acct", "newNonce" => "https://acme-v02.api.letsencrypt.org/acme/new-nonce", "newOrder" => "https://acme-v02.api.letsencrypt.org/acme/new-order", "revokeCert" => "https:/  /acme-v02.api.letsencrypt.org/acme/revoke-cert", "tNtPFfcQemQ" => "https://community.letsencrypt.org/t/adding-random-entries-to-the-directory/33417"}
+  directory_url: System.get_env("DIRECTORY_URL") || "https://acme-staging-v02.api.letsencrypt.org/directory",
+  account_key: System.get_env("ACCOUNT_KEY"),
+  account_kid: System.get_env("ACCOUNT_KID")
--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,8 @@
 import Config
 
-# config :logger, level: :debug
-# config :logger,
-#   level: :debug,
-#   backends: [:console],
-#   compile_time_purge_level: :debug
+config :acme_client,
+  account_key:
+    "{\"alg\":\"ES256\",\"crv\":\"P-256\",\"d\":\"CBbzro67SpHuMdKDvCdWlAGrVa-FPpFQYZWSPwwiO-4\",\"kty\":\"EC\",\"use\":\"sig\",\"x\":\"pZdm5JkVjULRH0RyJFxsc8BIXm0bRMHJBsuaN5aeSIA\",\"y\":\"GUWG_WobuxqZj6xpa3FC8zLIAA5UR0nptG3QO3d2dfM\"}"
 
 config :junit_formatter,
   report_dir: "#{Mix.Project.build_path()}/junit-reports",
