1a8c851f2d788f24f2da2e045090a4f425d8b535
Jake Morrison <jake@cogini.com>
2023-12-02 13:57:51 -0600

    Configure

--- config/dev.exs
+++ config/dev.exs
@@ -1,9 +1,28 @@
 import Config
 
-config :tesla, adapter: Tesla.Adapter.Hackney
+# config :acme_client, AcmeClient.Repo,
+#   username: System.get_env("DATABASE_USER") || "postgres",
+#   password: System.get_env("DATABASE_PASS") || "postgres",
+#   hostname: System.get_env("DATABASE_HOST") || "localhost",
+#   database: System.get_env("DATABASE_DB") || "acme_client_dev",
+#   stacktrace: true,
+#   show_sensitive_data_on_connection_error: true,
+#   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 config :acme_client,
   directory_url:
-    System.get_env("DIRECTORY_URL") || "https://acme-staging-v02.api.letsencrypt.org/directory",
-  account_key: System.get_env("ACCOUNT_KEY"),
-  account_kid: System.get_env("ACCOUNT_KID")
+    System.get_env("ACME_CLIENT_DIRECTORY_URL") || "https://acme-staging-v02.api.letsencrypt.org/directory",
+  account_key: System.get_env("ACME_CLIENT_ACCOUNT_KEY"),
+  account_kid: System.get_env("ACME_CLIENT_ACCOUNT_KID")
+
+config :logger, :default_formatter,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:file, :line]
+
+config :tesla, adapter: Tesla.Adapter.Hackney
+
+# config :tesla, Tesla.Middleware.Logger, debug: false
+# Logging configuration is evaluated at compile time, so Tesla must be
+# recompiled for the configuration to take effect:
+#   mix deps.clean --build tesla
+#   mix deps.compile tesla
--- config/test.exs
+++ config/test.exs
@@ -1,8 +1,30 @@
 import Config
 
+# config :acme_client, AcmeClient.Repo,
+#   username: System.get_env("DATABASE_USER") || "postgres",
+#   password: System.get_env("DATABASE_PASS") || "postgres",
+#   hostname: System.get_env("DATABASE_HOST") || "localhost",
+#   database: System.get_env("DATABASE_DB") || "acme_client_test#{System.get_env("MIX_TEST_PARTITION")}",
+#   pool: Ecto.Adapters.SQL.Sandbox,
+#   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
 config :acme_client,
   account_key:
     "{\"alg\":\"ES256\",\"crv\":\"P-256\",\"d\":\"CBbzro67SpHuMdKDvCdWlAGrVa-FPpFQYZWSPwwiO-4\",\"kty\":\"EC\",\"use\":\"sig\",\"x\":\"pZdm5JkVjULRH0RyJFxsc8BIXm0bRMHJBsuaN5aeSIA\",\"y\":\"GUWG_WobuxqZj6xpa3FC8zLIAA5UR0nptG3QO3d2dfM\"}"
+
+config :logger,
+  level: :warning,
+  always_evaluate_messages: true
+
+config :logger, :default_formatter,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:file, :line]
+
+config :tesla, Tesla.Middleware.Logger, debug: false
+# Logging configuration is evaluated at compile time, so Tesla must be
+# recompiled for the configuration to take effect:
+#   mix deps.clean --build tesla
+#   mix deps.compile tesla
 
 config :junit_formatter,
   report_dir: "#{Mix.Project.build_path()}/junit-reports",
