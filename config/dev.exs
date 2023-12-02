72060c1bd7b4fa9d7ecece6f572c2c4d6121f98f
Jake Morrison <jake@cogini.com>
2023-12-01 22:20:35 -0600

    mix format

--- config/dev.exs
+++ config/dev.exs
@@ -3,6 +3,7 @@ import Config
 config :tesla, adapter: Tesla.Adapter.Hackney
 
 config :acme_client,
-  directory_url: System.get_env("DIRECTORY_URL") || "https://acme-staging-v02.api.letsencrypt.org/directory",
+  directory_url:
+    System.get_env("DIRECTORY_URL") || "https://acme-staging-v02.api.letsencrypt.org/directory",
   account_key: System.get_env("ACCOUNT_KEY"),
   account_kid: System.get_env("ACCOUNT_KID")
