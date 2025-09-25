9668fa449a3a9c94a281fd20757e32ac3f15cbb8
Jake Morrison <jake@cogini.com>
2025-09-25 17:26:31 -0500

    Alphabetize

--- config/config.exs
+++ config/config.exs
@@ -6,11 +6,11 @@ import Config
 # config :acme_client, AcmeClient.Repo,
 #   database: Path.join(System.get_env("ACME_CLIENT_STATE_DIR", "/var/lib/acme-client"), "acme-client.db")
 
-config :logger,
-  level: :info
-
 config :logger, :default_formatter,
   format: "$time $metadata[$level] $message\n",
   metadata: [:pid, :module, :function, :line]
+
+config :logger,
+  level: :info
 
 import_config "#{config_env()}.exs"
